a2504a59180f1bc076d98d0e8f538d359cb8dd4b
AirHorizons <guardian1248@snu.ac.kr>
2016-07-14 13:18:41 +0900

    upload week02 homework

---
+++ Week2/Week2/Hanoi.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2
+{
+    class Hanoi
+    {
+        private HanoiTower[] towers;
+        public int NumOfDisks { get; private set; }
+        public int Turns { get; private set; }
+        public Hanoi()
+        {
+            Console.Write("원판의 개수를 입력하세요: ");
+            this.NumOfDisks = Convert.ToInt32(Console.ReadLine());
+            towers = new HanoiTower[3];
+
+            for (int i=0; i<towers.Length; i++)
+                towers[i] = new HanoiTower(NumOfDisks);
+            towers[0].InsertAllDisks();
+        }
+
+        public void Run()
+        {
+            Draw();
+            ExecuteTurn(NumOfDisks, towers[0], towers[1], towers[2]);
+            Console.WriteLine("총 {0}회의 이동으로 옮겼습니다.", Turns);
+        }
+        private void ExecuteTurn(int n, HanoiTower source, HanoiTower via, HanoiTower dest)
+        {
+            if (n==1)
+            {
+                dest.InsertDisk(source.RemoveDisk());
+                Draw();
+                Turns += 1;
+            }
+            else
+            {
+                ExecuteTurn(n - 1, source, dest, via);
+                dest.InsertDisk(source.RemoveDisk());
+                Draw();
+                Turns += 1;
+                ExecuteTurn(n - 1, via, source, dest);
+            }
+        }
+        public void Draw()
+        {
+            for (int i = NumOfDisks-1; i >= 0; i--)
+            {
+                for (int j = 0; j < towers.Length; j++)
+                {
+                    Console.Write(DrawDisk(NumOfDisks, towers[j].Disks[i]));
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+        }
+        private string DrawDisk(int n, int diskSize)
+        {
+            string str = "";
+            for (int i=0; i<2*n+1; i++)
+            {
+                if (i == n) str += "|";
+                else if (i < n - diskSize || i > n + diskSize) str += " ";
+                else str += "*"; 
+            }
+            str += "\t";
+            return str;
+        }
+    }
+}
---
+++ Week2/Week2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi hanoi = new Hanoi();
+            hanoi.Run();
+        }
+    }
+}
